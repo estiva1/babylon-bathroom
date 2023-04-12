91747d299ddd446c98c4c573a633438cc1da0681
Stanislav <solarcoaster.st@gmail.com>
2023-04-13 00:03:22 +0300

    minor refactoring

--- react/src/components/SceneComponent.js
+++ react/src/components/SceneComponent.js
@@ -35,7 +35,7 @@ const onSceneMount = (e) => {
 
   const loader = SceneLoader.ImportMeshAsync(
     "",
-    "https://raw.githubusercontent.com/estiva1/glb-models/master/",
+    "https://github.com/estiva1/babylon-bathroom/blob/main/glb-models/",
     "babylonBathroom.glb",
     scene,
     () => {
@@ -53,85 +53,85 @@ const onSceneMount = (e) => {
       const textures = [
         {
           name: "Tile 1 (30x30)",
-          url: "textures/tile1(30x30).jpg",
+          url: "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/tile1(30x30).jpg",
           width: "30",
           height: "30",
         },
         {
           name: "Tile 2 (30x30)",
-          url: "textures/tile2(30x30).jpg",
+          url: "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/tile2(30x30).jpg",
           width: "30",
           height: "30",
         },
         {
           name: "Tile 3 (60x30)",
-          url: "textures/tile4(60x30).jpg",
+          url: "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/tile4(60x30).jpg",
           width: "60",
           height: "30",
         },
         {
           name: "Tile 4 (30x60)",
-          url: "textures/tile4_1(30x60).jpg",
+          url: "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/tile4_1(30x60).jpg",
           width: "30",
           height: "60",
         },
         {
           name: "Tile 5 (30x30)",
-          url: "textures/tile4_2(30x30).jpg",
+          url: "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/tile4_2(30x30).jpg",
           width: "30",
           height: "30",
         },
         {
           name: "Tile 6 (30x30)",
-          url: "textures/tile5(30x30).jpg",
+          url: "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/tile5(30x30).jpg",
           width: "30",
           height: "30",
         },
         {
           name: "Tile 7 (30x60)",
-          url: "textures/tile5_1(30x60).jpg",
+          url: "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/tile5_1(30x60).jpg",
           width: "30",
           height: "60",
         },
         {
           name: "Tile 8 (30x30)",
-          url: "textures/tile6(30x30).jpg",
+          url: "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/tile6(30x30).jpg",
           width: "30",
           height: "30",
         },
         {
           name: "Tile 9 (60x60)",
-          url: "textures/tile6_1(60x60).jpg",
+          url: "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/tile6_1(60x60).jpg",
           width: "60",
           height: "60",
         },
         {
           name: "Tile 10 (30x30)",
-          url: "textures/tile7(30x30).jpg",
+          url: "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/tile7(30x30).jpg",
           width: "30",
           height: "30",
         },
         {
           name: "Tile 11 (60x60)",
-          url: "textures/tile9(60x60).jpg",
+          url: "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/tile9(60x60).jpg",
           width: "60",
           height: "60",
         },
         {
           name: "Tile 12 (fill)",
-          url: "textures/Water_Sp.jpg",
+          url: "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/Water_Sp.jpg",
           width: "_",
           height: "_",
         },
         {
           name: "Tile 13 (fill)",
-          url: "textures/Asphal01.jpg",
+          url: "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/Asphal01.jpg",
           width: "_",
           height: "_",
         },
         {
           name: "Tile 14 (fill)",
-          url: "textures/Grass_01.jpg",
+          url: "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/Grass_01.jpg",
           width: "_",
           height: "_",
         },
@@ -163,13 +163,21 @@ const onSceneMount = (e) => {
           const currentTileWidth = textures[i].width;
           const currentTileHeight = textures[i].height;
 
+          const scaleFactor = 10;
+          const standartTileWidth = 30;
+          const standartTileHeight = 30;
+
           icon.addEventListener("click", () => {
             if (selectedMesh.material) {
               selectedMesh.material.albedoTexture = textureObject;
               selectedMesh.material.albedoTexture.uScale =
-                currentTileWidth == 30 ? 10 : 5;
+                currentTileWidth == standartTileWidth
+                  ? scaleFactor
+                  : (standartTileWidth / currentTileWidth) * scaleFactor;
               selectedMesh.material.albedoTexture.vScale =
-                currentTileHeight == 30 ? 10 : 5;
+                currentTileHeight == standartTileHeight
+                  ? scaleFactor
+                  : (standartTileHeight / currentTileHeight) * scaleFactor;
             }
             popup.style.display = "none";
           });
