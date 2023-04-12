a334dc4d397b1265322d7ef7a7ba5232ca13bb2d
Stanislav <solarcoaster.st@gmail.com>
2023-04-12 21:20:44 +0300

    finalizing React app

--- react/src/components/SceneComponent.js
+++ react/src/components/SceneComponent.js
@@ -43,7 +43,7 @@ const onSceneMount = (e) => {
 
   const loader = SceneLoader.ImportMeshAsync(
     "",
-    "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/",
+    "https://raw.githubusercontent.com/estiva1/glb-models/master/",
     "babylonBathroom.glb",
     scene,
     () => {
@@ -58,19 +58,35 @@ const onSceneMount = (e) => {
       popup.style.display = "none";
       document.body.appendChild(popup);
 
+      // const textures = [
+      //   {
+      //     name: "Tile 1",
+      //     url: "https://raw.githubusercontent.com/estiva1/glb-models/main/tile1(30x30).jpg",
+      //     tileWidth: 30,
+      //     tileHeight: 30,
+      //   },
+      //   {
+      //     name: "Tile 2",
+      //     url: "https://raw.githubusercontent.com/estiva1/glb-models/main/tile2(30x30).jpg",
+      //     tileWidth: 30,
+      //     tileHeight: 30,
+      //   },
+      // ];
+
       const textures = [
-        {
-          name: "Tile 1",
-          url: "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/tile1(30x30).jpg",
-          tileWidth: 30,
-          tileHeight: 30,
-        },
-        {
-          name: "Tile 2",
-          url: "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/tile2(30x30).jpg",
-          tileWidth: 30,
-          tileHeight: 30,
-        },
+        "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/tile1(30x30).jpg",
+        "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/tile2(30x30).jpg",
+        "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/tile4_1(30x30).jpg",
+        "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/tile4_2(30x30).jpg",
+        "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/tile5(30x30).jpg",
+        "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/tile5_1(30x30).jpg",
+        "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/tile6(30x30).jpg",
+        "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/tile6_1(30x30).jpg",
+        "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/tile7(30x30).jpg",
+        "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/tile9(30x30).jpg",
+        "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/Water_Sp.jpg",
+        "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/Asphal01.jpg",
+        "https://raw.githubusercontent.com/estiva1/babylon-bathroom/main/glb-models/Grass_01.jpg",
       ];
 
       for (let i = 0; i < textures.length; i++) {
@@ -81,7 +97,7 @@ const onSceneMount = (e) => {
           textureContainer.style.padding = "0";
 
           const icon = document.createElement("img");
-          icon.src = textures[i].url; //URL
+          icon.src = textureObject.url;
 
           icon.style.width = "50px";
           icon.style.height = "50px";
@@ -92,13 +108,12 @@ const onSceneMount = (e) => {
           tileName.style.margin = "0";
           tileName.style.fontSize = "10px";
 
-          const tileTexture = textures[i].name; //name
-          tileName.innerHTML = tileTexture;
+          tileName.innerHTML = "Tile " + i + 1;
           textureContainer.appendChild(icon);
           textureContainer.appendChild(tileName);
 
-          const currentTileWidth = textures[i].tileWidth;
-          const currentTileHeight = textures[i].tileHeight;
+          const currentTileWidth = textures[i].slice(-11).slice(1, -8);
+          const currentTileHeight = textures[i].slice(-11).slice(4, -5);
 
           icon.addEventListener("click", () => {
             if (selectedMesh.material) {
